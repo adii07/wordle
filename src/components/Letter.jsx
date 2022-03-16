72f6aaf45db26a5ffd2d0a9fe253d1d9d02e346f
adii07 <adityasharma5119@gmail.com>
2022-03-17 03:36:24 +0530

    useContext hook

--- src/components/Letter.jsx
+++ src/components/Letter.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
-
+import { useContext } from 'react';
+import { AppContext } from '../App';
 function Letter({letterPos,attemptVal}) {
+    const {board,useBoard}=useContext(AppContext);
     const letter=board[letterPos][attemptVal];
   return (
     <div className='letter'>{letter}</div>
